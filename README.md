a6388f91be43da55d7cdf2ea31ed969736450a11
wysiwyg-jax <jan_1@posteo.de>
2023-03-13 13:28:22 +0100

    added function to makefilelist + randomly pick one image from four, edited renderpage, readme. deleted unnecessary calendar functions from template.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # tearoffcalendar
-tearoffcalendar
+tearoffcalendar is a simple web component that contains a tear off calendar animation. despite its name tearoffcalendar does not offer any calendar data but instead gives users the possibility to select pictures to appear in the calendar in random order. reusability in this case does not mean reuseability for webdevs but for designers, for they can simply put imgs in folders and change all styling in their guis.
+
+# use
+insert all pictures that shall appear in the calendar in the "img"-folder in svg-format. also place first and last image in folder "imgfirstlast" and name the img that shall appear first "first.svg" and the last img "last.svg".
--- script.js
+++ script.js
@@ -1,38 +1,26 @@
 const pages = document.querySelector('.pages');
-const locale = window.navigator.language || 'en-us';
 
-// let date = new Date();
-// let dayNum = date.getDate();
-// let month = date.getMonth();
-// let dayName = date.toLocaleString(locale, { weekday: 'long' });
-// let monthName = date.toLocaleString(locale, { month: 'long' });
-// let year = date.getFullYear();
-
-// function daysInMonth(month, year) {
-//   return new Date(year, month + 1, 0).getDate();
-// }
-
-// function getNewDate() {
-//   if (dayNum < daysInMonth(month, year)) {
-//     dayNum++;
-//   } else {
-//     dayNum = 1;
-//   }
-//   if (dayNum === 1 && month < 11) {
-//     month++;
-//   } else if (dayNum === 1 && month === 11) {
-//     month = 0;
-//   }
-//   if (dayNum === 1 && month === 0) {
-//     year++;
-//   }
-//   const newDate = new Date(year, month, dayNum);
-//   dayName = newDate.toLocaleString('en-us', { weekday: 'long' });
-//   monthName = newDate.toLocaleString('en-us', { month: 'long' });
-// }
+// generate matrix for the path + filenames. then randomize
+function makeFileList(){
+  let filenameList = [];  
+  for (let i = 0; i < 26; i++){
+    let sublist = []
+    for (let j = 0; j < 4; j++){
+      let currFilename = "img/" + String.fromCharCode((97+i)) + "-" + (j+1)  + ".svg"
+      sublist.push(currFilename)
+    };
+    filenameList.push(sublist)
+  };
+  let randomizedList = []
+  for (let i = 0; i < 26; i++){
+    let randomElement = (filenameList[i])[Math.floor(Math.random() * (filenameList[i]).length)];
+    randomizedList.push(randomElement)
+  };
+  return randomizedList;
+};
 
 function handleClick(e) {
-  getNewDate();
+  // TODO: if last page do nothing
   updateCalendar(e.target);
 }
 
@@ -49,17 +37,30 @@ function updateCalendar(target) {
 }
 
 function renderPage() {
+  let currentSrc; 
+  // pick curr svg
+  if (renderPageCallCounter === 0){
+   currentSrc = "imgfirstlast/last.svg";
+  } else if(renderPageCallCounter > filenames.length){
+   currentSrc = "imgfirstlast/first.svg";
+  } else {
+   currentSrc = filenames[renderPageCallCounter-1];
+  }
+  currentSrc = "img/loose_leaf_paper.svg"
+
   const newPage = document.createElement('div');
   newPage.classList.add('page');
   newPage.innerHTML = `
-    <p class="month">${monthName}</p>
-    <p class="day">${dayNum}</p>
-    <p class="day-name">${dayName}</p>
-    <p class="year">${year}</p>
+    <img src="${currentSrc}"></img>
   `;
+    
   pages.appendChild(newPage);
+  renderPageCallCounter++;
 }
+//<img src='Folder/`+$(filenames[renderPageCallCounter-1])+`></img>
 
+const filenames = makeFileList();
+var renderPageCallCounter = 0;
 renderPage();
 
 pages.addEventListener('click', handleClick);
