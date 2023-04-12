228929939f8374dff9f451a2fbebd95433e991c4
wysiwyg-jax <jan_1@posteo.de>
2023-04-12 17:36:36 +0200

    modified gitignore, deleted script exports

--- script/handleClick.js
+++
@@ -1,8 +0,0 @@
-import {renderPageCallCounter} from "/tearoffscript.js"
-
-function handleClick( e ) {
-        if( renderPageCallCounter < randomfiles.length ){
-          updateCalendar( e.target );
-        };
-    };
-export default handleClick;
--- script/imprintbtn.js
+++
@@ -1,7 +0,0 @@
-function imprintbtn(){
-      for ( let i = 0; i < randomfiles.length; i++ ){
-        document.querySelectorAll('.page:not(.tear)')[0].click();
-      }
-      renderPageCallCounter = randomfiles.length;
-    };
-    export default imprintbtn;
